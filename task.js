413a1f64042cf5a83ddc094b8f3e79274e967e2c
Abdullah Karaman <abdullahkaramaniletisim@gmail.com>
2023-01-21 19:25:15 +0300

    gitignore + tailwind fixed

--- task.js
+++ task.js
@@ -1,14 +1,10 @@
-import { Application } from '@splinetool/runtime';
+import { Application } from "@splinetool/runtime";
 
-
-
-
-const canvascool = document.getElementById('canvascool');
+const canvascool = document.getElementById("canvascool");
 const app_cool = new Application(canvascool);
-app_cool.load('https://prod.spline.design/cHYcy3yVGTYQT3kw/scene.splinecode');
+app_cool.load("https://prod.spline.design/cHYcy3yVGTYQT3kw/scene.splinecode");
 
 // TASK EMOJI 3d
-const canvas = document.getElementById('canvas3d');
+const canvas = document.getElementById("canvas3d");
 const app = new Application(canvas);
-app.load('https://prod.spline.design/ahjKx5OFCLtVZl2f/scene.splinecode');
-
+app.load("https://prod.spline.design/ahjKx5OFCLtVZl2f/scene.splinecode");
