31d25cb98cfdcb318e9b746de6e3570eebf009a9
Abdullah Karaman <abdullahkaramaniletisim@gmail.com>
2023-01-21 19:01:13 +0300

    Revert "gitignore added and tailwind fixed"
    This reverts commit ce72eb92146a1fe89a4136ac864561779d68b5d2.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,44 +1,46 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./**/*.{html,js}", "./node_modules/flowbite/**/*.js"],
+  content: ['./src/**/*.{html,js}', "./node_modules/flowbite/**/*.js"],
     safelist: [
-        "text-2xl",
-        "text-3xl",
-        "text-4xl",
-        "text-5xl",
-        "text-6xl",
-        "sm:text-2xl",
-        "sm:text-3xl",
-        "sm:text-4xl",
-        "sm:text-5xl",
-        "sm:text-6xl",
-        "md:text-2xl",
-        "md:text-3xl",
-        "md:text-4xl",
-        "md:text-5xl",
-        "md:text-6xl",
-        "lg:text-2xl",
-        "lg:text-3xl",
-        "lg:text-4xl",
-        "lg:text-5xl",
-        "lg:text-6xl",
+        'text-2xl',
+        'text-3xl',
+        'text-4xl',
+        'text-5xl',
+        'text-6xl',
+        'sm:text-2xl',
+        'sm:text-3xl',
+        'sm:text-4xl',
+        'sm:text-5xl',
+        'sm:text-6xl',
+        'md:text-2xl',
+        'md:text-3xl',
+        'md:text-4xl',
+        'md:text-5xl',
+        'md:text-6xl',
+        'lg:text-2xl',
+        'lg:text-3xl',
+        'lg:text-4xl',
+        'lg:text-5xl',
+        'lg:text-6xl',
     ],
-    theme: {
-        colors: {
-            primary: "#1D1D1B",
-            primary_light: "#353532",
-            accent: "#AAFF17",
-            accent_light: "#D4FF5B",
-            secondary: "#2037BA",
-            white: "#FFFFFF",
-            bg: "#EDE7E2",
-            purple: "#592270",
-        },
-
-        fontFamily: {
-            sans: ["Poppins", "sans-serif"],
-        },
-        extend: {},
+  theme: {
+    colors: {
+      primary: '#1D1D1B',
+      primary_light: '#353532',
+      accent: '#AAFF17',
+      accent_light: '#D4FF5B',
+      secondary: '#2037BA',
+      white: '#FFFFFF',
+      bg: '#EDE7E2',
+      purple: '#592270',
     },
-    plugins: [require("flowbite/plugin")],
-};
+
+    fontFamily: {
+      sans: ['Poppins', 'sans-serif'],
+    },
+    extend: {}
+  },
+  plugins: [
+    require('flowbite/plugin')
+  ],
+}
